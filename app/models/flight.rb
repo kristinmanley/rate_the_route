6ec64ab299734473c9d2a85aeaaf3ab5b0d26dad
Raghu Betina <raghu@firstdraft.com>
2017-03-08 20:16:20 +0000

    Direct association between flights and reviews

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,9 @@
 class Flight < ApplicationRecord
   # Direct associations
 
+  has_many   :reviews,
+             :dependent => :destroy
+
   belongs_to :destination_city
 
   belongs_to :departure_city
