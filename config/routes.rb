79e12651be7427711df70387c9340852a99c2e93
Raghu Betina <raghu@firstdraft.com>
2017-03-08 20:16:37 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "airline_carriers#index"
   # Routes for the Departure_airport resource:
   # CREATE
