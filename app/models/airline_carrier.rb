0aed56f288d301a524c1130f6b850e3bb57d41fc
Raghu Betina <raghu@firstdraft.com>
2017-03-08 20:16:20 +0000

    Direct association between airline_carriers and flights

--- app/models/airline_carrier.rb
+++ app/models/airline_carrier.rb
@@ -1,6 +1,10 @@
 class AirlineCarrier < ApplicationRecord
   # Direct associations
 
+  has_many   :flights,
+             :foreign_key => "airline_id",
+             :dependent => :destroy
+
   # Indirect associations
 
   # Validations
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,9 @@
 class Flight < ApplicationRecord
   # Direct associations
 
+  belongs_to :airline,
+             :class_name => "AirlineCarrier"
+
   # Indirect associations
 
   # Validations
